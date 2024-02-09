103fa3e66d384e4701ae191289167fe6714d8bce
geo1310 <gbriche59@yahoo.fr>
2024-02-09 11:23:08 +0100

    Creation et tests Admin des modeles

--- litrevu/bookreview/admin.py
+++ litrevu/bookreview/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from bookreview.models import Ticket, Review, UserFollows
 
-# Register your models here.
+
+class TicketAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'user', 'time_created')
+
+
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('headline', 'ticket', 'rating', 'user', 'time_created')
+
+
+class UserFollowsAdmin(admin.ModelAdmin):
+    list_display = ('user', 'followed_user')
+
+
+admin.site.register(Ticket, TicketAdmin)
+admin.site.register(Review, ReviewAdmin)
+admin.site.register(UserFollows, UserFollowsAdmin)
--- litrevu/bookreview/models.py
+++ litrevu/bookreview/models.py
@@ -11,6 +11,9 @@ class Ticket(models.Model):
     image = models.ImageField(verbose_name='ticket image')
     time_created = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return self.title
+
 
 class Review(models.Model):
     ticket = models.ForeignKey(to=Ticket, on_delete=models.CASCADE)
@@ -18,7 +21,7 @@ class Review(models.Model):
         # validates that rating must be between 0 and 5
         validators=[MinValueValidator(0), MaxValueValidator(5)])
     headline = models.CharField(max_length=128)
-    body = models.CharField(max_length=8192, blank=True)
+    body = models.TextField(max_length=8192, blank=True)
     user = models.ForeignKey(
         to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     time_created = models.DateTimeField(auto_now_add=True)
