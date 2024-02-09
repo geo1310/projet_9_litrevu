4258c6f615e1e798b64dd576a94e2b27f4cb17bb
geo1310 <gbriche59@yahoo.fr>
2024-02-09 10:03:21 +0100

    Création des modeles et migration pour bookreview

--- litrevu/authentication/models.py
+++ litrevu/authentication/models.py
@@ -2,5 +2,4 @@ from django.contrib.auth.models import AbstractUser
 
 
 class CustomUser(AbstractUser):
-    # Vous n'avez pas besoin d'ajouter de champs supplémentaires
     pass
--- litrevu/bookreview/models.py
+++ litrevu/bookreview/models.py
@@ -1,3 +1,35 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Ticket(models.Model):
+    # Your Ticket model definition goes here
+    title = models.CharField(max_length=128)
+    description = models.TextField(max_length=2048, blank=True)
+    user = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    image = models.ImageField(verbose_name='ticket image')
+    time_created = models.DateTimeField(auto_now_add=True)
+
+
+class Review(models.Model):
+    ticket = models.ForeignKey(to=Ticket, on_delete=models.CASCADE)
+    rating = models.PositiveSmallIntegerField(
+        # validates that rating must be between 0 and 5
+        validators=[MinValueValidator(0), MaxValueValidator(5)])
+    headline = models.CharField(max_length=128)
+    body = models.CharField(max_length=8192, blank=True)
+    user = models.ForeignKey(
+        to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    time_created = models.DateTimeField(auto_now_add=True)
+
+
+class UserFollows(models.Model):
+    # Your UserFollows model definition goes here
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='following')
+    followed_user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='followed_by')
+
+    class Meta:
+        # ensures we don't get multiple UserFollows instances
+        # for unique user-user_followed pairs
+        unique_together = ('user', 'followed_user', )
